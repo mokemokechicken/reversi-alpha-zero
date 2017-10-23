d6ed22be4c65e7002b9966bca4cf68f7365b08a5
Ken Morishita <mokemokechicken@gmail.com>
2017-10-23 14:00:29 +0900

    wip

--- src/reversi_zero/play_game/game_model.py
+++ src/reversi_zero/play_game/game_model.py
@@ -1,12 +1,13 @@
 from reversi_zero.config import Config
 from reversi_zero.env.reversi_env import Player, ReversiEnv
+from reversi_zero.lib.bitboard import find_correct_moves
 
 
 class PlayWithHuman:
     def __init__(self, config: Config):
         self.config = config
         self.over = True
-        self.human_is_black = None
+        self.human_color = None
         self.observers = []
         self.env = ReversiEnv().reset()
 
@@ -14,7 +15,7 @@ class PlayWithHuman:
         self.observers.append(observer_func)
 
     def start_game(self, human_is_black):
-        self.human_is_black = human_is_black
+        self.human_color = Player.black if human_is_black else Player.white
         self.env = ReversiEnv().reset()
 
     @property
@@ -32,12 +33,21 @@ class PlayWithHuman:
             return Player.white
         return None
 
-    def available(self, px, py, for_human=True):
+    def available(self, px, py):
         pos = int(py * 8 + px)
         if pos < 0 or 64 <= pos:
             return False
+        own, enemy = self.env.board.black, self.env.board.white
+        if self.human_color == Player.white:
+            own, enemy = enemy, own
+        legal_moves = find_correct_moves(own, enemy)
+        return legal_moves & (1 << pos)
 
-    def move(self, px, py, by_human=True):
+    def move(self, px, py):
         pos = int(py * 8 + px)
         assert 0 <= pos < 64
 
+        if self.next_player != self.human_color:
+            return False
+
+        self.env.step(pos)
